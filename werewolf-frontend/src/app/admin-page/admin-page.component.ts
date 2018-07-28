4fe34ff013a8fe20a7a973fbe94b8aba5b93edca
Isadora Bassetto Kwiatkowski <isadorabk@gmail.com>
2018-07-28 11:29:43 +0200

    feat: assign werewolves

--- werewolf-frontend/src/app/admin-page/admin-page.component.ts
+++ werewolf-frontend/src/app/admin-page/admin-page.component.ts
@@ -21,8 +21,37 @@ export class AdminPageComponent implements OnInit {
     this.gameId = this.apiClientService.getGameId();
     this.apiClientService.getPlayers(this.gameId)
       .subscribe(data => {
-        this.players = data;
+        this.players = this.assignWerewolves(data);
       })
   }
+
+  assignWerewolves(data): Player[] {
+    let playersRole = data.slice();
+    const numOfPlayers = playersRole.length;
+    const werewolvesRatio = 4;
+    const numOfWerewolves = Math.floor(numOfPlayers / werewolvesRatio);
+    const werewolves = this.getRandomElements(playersRole, numOfWerewolves);
+    playersRole.forEach(player => {
+      werewolves.forEach(werewolf => {
+        if (werewolf.playerId === player.playerId) player.role = 'werewolf';
+      });
+    });
+    return playersRole;
+  }
+
+getRandomElements(arr, n): Player[] {
+  let result = new Array(n);
+  let len = arr.length;
+  let taken = new Array(len);
+  if (n > len) throw new RangeError('getRandomElements: more elements taken than avaiable')
+  while (n--) {
+    let x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
+
+
 
 }
